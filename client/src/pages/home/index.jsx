eaefe1d30c63588d663edd77f1f8f0dcf68c02bc
baker-b <akalishuk@gmail.com>
2022-12-08 18:36:20 +0200

    client app uploader added

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,13 +1,41 @@
 import React from "react";
-
-function HomePage() {
-  return (
-    <>
-      <h1>File upload will be here</h1>
-
-      <p>on the file src/pages/home/index</p>
-    </>
-  );
-}
-
+import { message, Upload } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
+const { Dragger } = Upload;
+const props = {
+  name: "file",
+  multiple: true,
+  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (status === "done") {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log("Dropped files", e.dataTransfer.files);
+  },
+};
+const HomePage = () => (
+  <>
+    <h1>Uploader</h1>
+    <Dragger {...props}>
+      <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">
+        Click or drag file to this area to upload
+      </p>
+      <p className="ant-upload-hint">
+        Support for a single or bulk upload. Strictly prohibit from uploading
+        company data or other band files
+      </p>
+    </Dragger>
+  </>
+);
 export default HomePage;
